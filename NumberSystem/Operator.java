77993b7f5dc921c6976ffd2c8c9aeb8e9737d1cf
uday <udaybhanyadav73072@gmail.com>
2025-05-11 15:40:05 +0530

    Add solution for [ Set i th bit ]

--- NumberSystem/Operator.java
+++ NumberSystem/Operator.java
@@ -1,8 +1,16 @@
 class Operator {
   public static void main(String[] args) {
+
+    // Check i th bit is set or not
     int a = 5;
     int i = 1;
     boolean check = ((a >> i) & 1) == 1;
-    System.out.println("hello");
+    System.out.println(" 1 th bit of 5 is set or not = " + check);
+
+    // Set i th bit
+    int number = 4;
+    int position = 1;
+    int result = (number | (1 << position));
+    System.out.println(result);
   }
 }
