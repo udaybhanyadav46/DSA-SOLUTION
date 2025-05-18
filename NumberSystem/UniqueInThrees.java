761b8f064a6425e8ab48d14f3968d80af712e23a
uday <udaybhanyadav73072@gmail.com>
2025-05-18 13:30:03 +0530

    Add second solution for find unique element in  three

--- NumberSystem/UniqueInThrees.java
+++ NumberSystem/UniqueInThrees.java
@@ -1,6 +1,18 @@
 public class UniqueInThrees {
+
+  public static int secondWay(int arr[]) {
+    int one = 0;
+    int two = 0;
+
+    for (int i = 0; i < arr.length; i++) {
+      one = (one ^ arr[i]) & ~two;
+      two = (two ^ arr[i]) & ~one;
+    }
+    return one;
+  }
+
   public static void main(String[] args) {
-    int arr[] = { 1, 1, 1, 7, 3, 3, 3, 2,2,2};
+    int arr[] = { 1, 1, 1, 9, 3, 3, 3, 2, 2, 2 };
     int ans = 0;
     for (int bitIndex = 0; bitIndex < 32; bitIndex++) {
       int count = 0;
@@ -14,5 +26,7 @@ public class UniqueInThrees {
       }
     }
     System.out.println(ans);
+
+    System.out.println(secondWay(arr));
   }
 }
